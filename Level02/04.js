6ca0cd3bbeca5eb9dbfe8f7b45da5a6a9c8a7ac3
taeyoung <her020212@naver.com>
2024-12-21 13:35:42 +0900

    탭바정리

--- Level02/04.js
+++ Level02/04.js
@@ -1,8 +1,10 @@
 // products를 클릭하면 products를 제외한 모든 태그의의 오렌지와 쇼 태그를 제거한다.
 // 클릭한 곳에 오렌지 태그와 쇼태그를 붙인다.
 // 클리스리스트 에서 클래서 제거하거나 추가할때 .을 붙이면 안된다.
+// array.forEach((니가정한변수)=>{ 니가정한변수.함수붙일거 쓰셈})
+// 이러면 앞에 어레이에서 하나씩꺼내서 니가정한 변수로 대체됨
 
-for (let i = 0; i <= 2; i++) {
+for (let i = 0; i <= document.querySelectorAll(".tab-button").length; i++) {
   document.querySelectorAll(".tab-button")[i].addEventListener("click", () => {
     document.querySelectorAll(".tab-button").forEach((button) => {
       button.classList.remove("orange");
