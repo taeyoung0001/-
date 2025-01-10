16348c9fc0b7f5927ee204c30e5e467bcc93eace
taeyoung <her020212@naver.com>
2025-01-10 18:23:33 +0900

    json파일 연결 완료

--- prectice/01/cody.js
+++ prectice/01/cody.js
@@ -1,12 +1,17 @@
-fetch("./cody.json")
+//html이 있는 경로를 기준으로 상대경로를 잡아야함
+const titleText = document.querySelector(".titleText");
+
+fetch("../cody.json")
   .then((res) => {
     if (!res.ok) {
       throw new Error(`네트워크 응답이 실패했습니다: ${res.status}`);
     }
     return res.json();
   })
+
   .then((data) => {
-    console.log(data);
+    console.log(data.temp[0].minusTemp);
+    titleText.innerHTML = data.temp[0].minusTemp.title;
   })
   .catch((error) => {
     console.error("Fetch 오류:", error);
